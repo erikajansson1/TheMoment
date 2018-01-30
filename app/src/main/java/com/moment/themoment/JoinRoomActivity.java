e1da69a43e41327f1dc36288631f848385d229bf
Fredrik Söderberg <soderbergh@gmail.com>
2018-01-30 20:53:48 +0100

    created empty ResultPage created empty VoteOnClaim created empty WriteClaim

--- app/src/main/java/com/moment/themoment/JoinRoomActivity.java
+++ app/src/main/java/com/moment/themoment/JoinRoomActivity.java
@@ -24,30 +24,17 @@ public class JoinRoomActivity extends AppCompatActivity {
         String userNameString = userName.getText().toString();
 
         Boolean joinedSuccesfully = FALSE;
-        Boolean myTurn = FALSE;
-        //TODO Query server for room existence and if possible add client to Room. If succesfull return true
-        //TODO check if user is to be prompted for input or awaits other users input, true or false
-
-        if (joinedSuccesfully && myTurn) {
-            jumpToWriteClaim(view);
-        }
-        else if (joinedSuccesfully && !myTurn) {
+        //TODO Query server for room existence and if possible add client to Room. If succesfull return true, do a callback to jumpToWaitForClaim
+        if (joinedSuccesfully) {
             jumpToWaitForClaim(view);
         }
         else {
             //TODO if failure, handle failure, error message? response from server error message maybe?
         }
-
-
+        jumpToWaitForClaim(view);
     }
-
     private void jumpToWaitForClaim(View view) {
-        //TODO Intent intent = new Intent(this, waitForClaim.class);
-        //startActivity(intent);
-    }
-
-    private void jumpToWriteClaim (View view) {
-        //TODO Intent intent = new Intent(this, writeClaim.class);
-        //startActivity(intent);
+        Intent intent = new Intent(this, waitForClaim.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/moment/themoment/WaitForClaim.java
+++ app/src/main/java/com/moment/themoment/WaitForClaim.java
@@ -3,14 +3,16 @@ package com.moment.themoment;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Boolean.FALSE;
 
-public class waitForClaim extends AppCompatActivity {
+public class WaitForClaim extends AppCompatActivity {
     TextView timeCount;
+    ProgressBar claimProgress;
     private static final String FORMAT = "%02d";
     int seconds , minutes, numberOfPlayers, roomSize;
 
@@ -18,13 +20,16 @@ public class waitForClaim extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wait_for_claim);
-        timeCount= findViewById(R.id.countDown);
+        timeCount = findViewById(R.id.countDown);
+        claimProgress = findViewById(R.id.progress);
         queryDBWriteClaim();
         new CountDownTimer(30000, 1000) {
             public void onTick(long millisUntilFinished) {
                 timeCount.setText(""+String.format(FORMAT,
                         TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
                                 TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+
+                claimProgress.setProgress(claimProgress.getProgress()+1);
             }
             public void onFinish() {
                 //TODO if countdown reaches zero probably call server for another check on whom are to writeclaim
