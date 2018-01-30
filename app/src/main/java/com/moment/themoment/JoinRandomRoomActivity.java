bace91ccf8ee819d77bcec30493c90339ec65ae5
Fredrik Söderberg <soderbergh@gmail.com>
2018-01-30 18:23:31 +0100

    strutured join room and added known TODO's

--- app/src/main/java/com/moment/themoment/JoinRandomRoomActivity.java
+++ app/src/main/java/com/moment/themoment/JoinRandomRoomActivity.java
@@ -1,7 +1,11 @@
 package com.moment.themoment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static java.lang.Boolean.FALSE;
 
 public class JoinRandomRoomActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class JoinRandomRoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_random_room);
+        //TODO Add loading symbol
+        //TODO Make server call for random room
+        //TODO Handle failure by reacting on error message and kick user back to main menu
+        Boolean foundRoom = FALSE;
+
+        if (foundRoom) {
+            jumpToWaitForClaim();
+        }
+        else {
+            finish();
+        }
+    }
+
+    private void jumpToWaitForClaim() {
+        //TODO Intent intent = new Intent(this, waitForClaim.class);
+        //startActivity(intent);
     }
 }
