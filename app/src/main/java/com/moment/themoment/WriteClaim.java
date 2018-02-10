bd73174bc0e1cdb89dd0b644dc72940f1e99bcd3
erikajansson1 <erika_jansson1@hotmail.com>
2018-02-10 20:46:43 +0100

    Fixed functionality to find out which radiobutton being chosen, also fixed what happens if no radiobutton is selected. Functionality for reading the claim is also added and prepared for being sent to server when generic sending functionality exists. Trello card "Write claim  - setup necessary functionality and network coms".

--- app/src/main/java/com/moment/themoment/WriteClaim.java
+++ app/src/main/java/com/moment/themoment/WriteClaim.java
@@ -1,27 +1,53 @@
 package com.moment.themoment;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 
 public class WriteClaim extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.TheMoment.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_claim);
     }
 
-    /* public void saveClaim(View view) {
-        Intent intent = new Intent(this, VoteOnClaim.class);
-        EditText claim = (EditText) findViewById(R.id.theClaim);
+    public void saveClaim(View view) {
+        EditText claim = findViewById(R.id.theClaim);
         String message = claim.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+
+        RadioGroup theGroup = findViewById(R.id.TrueFalseGroup);
+
+        if(theGroup.getCheckedRadioButtonId()==-1)
+        {
+            Toast.makeText(getApplicationContext(), "Please pick a correct answer", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            int selectedId = theGroup.getCheckedRadioButtonId();
+            RadioButton selectedRadioButton = (RadioButton) findViewById(selectedId);
+            goToVoteOnClaim(view);
+        }
+
+        //TODO: Send message to server, save it to DB so it can be showed to all players during the round.
+        //TODO: Also send selectedRadioButton so we can keep track of the correct answer.
+        //TODO: goToVoteOnClaim should link to the, by server, chosen claim to answer at, randomly picked out of all players claims, all players should get the claims in the same order.
+
+
+
+    }
+
+    private void goToVoteOnClaim(View view) {
+        Intent intent = new Intent(this, VoteOnClaim.class);
         startActivity(intent);
     }
-    */
+
 }
+
+
