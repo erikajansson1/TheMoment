0c367b3593486c6c3fde4fffd39078a016f1485c
DeeAwesomeOne <Desiree.Bjorkman.4477@student.uu.se>
2018-02-13 13:20:09 +0100

    Remove button, added some smaller functions to the room, removed claim.

--- app/src/main/java/com/moment/themoment/Claim.java
+++
@@ -1,4 +0,0 @@
-package com.moment.themoment;
-
-public class Claim {
-}
--- app/src/main/java/com/moment/themoment/Player.java
+++ app/src/main/java/com/moment/themoment/Player.java
@@ -7,6 +7,7 @@ public class Player {
     private String name;
     private Boolean answer;
     private int score;
+    private String claim;
 
     /*
     * Creates a person, used right now just for basic concept in Json building
@@ -15,5 +16,52 @@ public class Player {
         this.name = name;
         this.answer = false;
         this.score = 0;
+        this.claim = null;
     }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public Boolean getAnswer(){
+        return this.answer;
+    }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public String getClaim(){
+        return this.claim;
+    }
+
+
+    public void setAnswer(Boolean givenAnswer){
+        this.answer = givenAnswer;
+        return;
+    }
+
+    /**
+     * Will change score to given int
+     * @param givenScore
+     */
+    public void setScore(int givenScore){
+        this.score = givenScore;
+        return;
+    }
+
+    /**
+     * Adds one point to the score
+     */
+    public void addScore(){
+        this.score += 1;
+        return;
+    }
+
+    public void setClaim(String givenClaim){
+        this.claim = givenClaim;
+    }
+
+
+
 }
