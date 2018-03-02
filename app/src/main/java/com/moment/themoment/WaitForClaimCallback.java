5cd2dfc10df220cf5e8e5f48435b3a1c23abad96
DeeAwesomeOne <Desiree.Bjorkman.4477@student.uu.se>
2018-03-02 22:22:28 +0100

    Added functions in the interface

--- app/src/main/java/com/moment/themoment/WaitForClaimCallback.java
+++ app/src/main/java/com/moment/themoment/WaitForClaimCallback.java
@@ -1,8 +1,9 @@
 package com.moment.themoment;
 
-/**
- * Created by Nhunter on 2018-03-01.
- */
-
 public interface WaitForClaimCallback {
+
+    void updateWaitForClaim(String response);
+
+    void updateCurrentClaim(Claim claim);
+
 }
