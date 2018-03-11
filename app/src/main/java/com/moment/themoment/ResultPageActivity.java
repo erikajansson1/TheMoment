3a196e42994d51d8fb4b008675461bc9e5260d78
Fredrik Söderberg <soderbergh@gmail.com>
2018-03-11 23:06:12 +0100

    Players quiting mid-game could potentially break syncro....now fixed, probably!

--- app/src/main/java/com/moment/themoment/ResultPageActivity.java
+++ app/src/main/java/com/moment/themoment/ResultPageActivity.java
@@ -267,6 +267,14 @@ public class ResultPageActivity extends AppCompatActivity implements ResultPageC
         //TODO if its ClientPlayers claim next user will be blocked here!
         Intent intent;
         if (currentRoom.getCurrentClaimNo() != 0) {
+            if (currentRoom.getCurrentClaimNo() >= currentRoom.getPlayerList().size()) {
+                intent = new Intent(this, ResultPageActivity.class);
+                intent.putExtra("playerData", clientPlayer);
+                intent.putExtra("roomData", currentRoom);
+                startActivity(intent);
+                //TODO neaten up but this should keep the syncro
+                return;
+            }
             if (clientPlayer.claimIsClients(currentRoom.getCurrentClaim().getID())) {
                 this.myClaimIsNow();
                 return;
